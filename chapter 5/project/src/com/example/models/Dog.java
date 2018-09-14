407e167c85988c3b5fe8f900bc259c726fea2edd
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2018-09-14 19:09:23 +0300

    chapter 5 -> session binding listener

--- chapter 5/project/src/com/example/models/Dog.java
+++ chapter 5/project/src/com/example/models/Dog.java
@@ -1,9 +1,12 @@
 package com.example.models;
 
+import javax.servlet.http.HttpSessionBindingEvent;
+import javax.servlet.http.HttpSessionBindingListener;
+
 /**
  * Created by Dmitrii on 11.09.2018.
  */
-public class Dog {
+public class Dog implements HttpSessionBindingListener {
     private String breed;
 
     public Dog(String breed) {
@@ -16,5 +19,15 @@ public class Dog {
 
     public void setBreed(String breed) {
         this.breed = breed;
+    }
+
+    @Override
+    public void valueBound(HttpSessionBindingEvent httpSessionBindingEvent) {
+        System.out.println("Value was bounded to session");
+    }
+
+    @Override
+    public void valueUnbound(HttpSessionBindingEvent httpSessionBindingEvent) {
+        System.out.println("Value was unbounded from session");
     }
 }
--- chapter 5/project/src/com/example/web/MySessionBindingServlet.java
+++ chapter 5/project/src/com/example/web/MySessionBindingServlet.java
@@ -2,7 +2,6 @@ package com.example.web;
 
 import com.example.models.Dog;
 
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,21 +11,20 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 /**
- * Created by Dmitrii on 11.09.2018.
+ * Created by Dmitrii on 14.09.2018.
  */
-public class ServletContextListenerTest extends HttpServlet {
-
+public class MySessionBindingServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
         PrintWriter writer = response.getWriter();
+        writer.println("<h1>Session dog test</h1>");
 
-        ServletContext context = getServletContext();
-        Dog dog = (Dog) context.getAttribute("dog");
         HttpSession session = request.getSession();
-        session.setAttribute("dog", dog);
-
-        writer.println("<h1>Servlet context parameter dog</h1>");
-        writer.println("<h2>Dog's breed is: " + dog.getBreed() + "</h2>");
+        Object dog = session.getAttribute("dog");
+        if(dog instanceof Dog) {
+            Dog trueDog = (Dog) dog;
+            writer.println("<p>Dog's breed is : " + trueDog.getBreed() + "</p>" );
+        }
     }
 }

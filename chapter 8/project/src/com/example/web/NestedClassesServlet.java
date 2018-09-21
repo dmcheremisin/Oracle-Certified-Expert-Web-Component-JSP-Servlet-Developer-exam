857b74b1689326413b6f3f176ae39172d2917f0b
dmitrii89 <dmitry_cher@mail.ru>
2018-09-21 11:17:53 +0300

    chapter 8: jsp expression language with map object

--- chapter 8/project/src/com/example/web/NestedClassesServlet.java
+++ chapter 8/project/src/com/example/web/NestedClassesServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class NestedClassesServlet extends HttpServlet {
     @Override
@@ -20,7 +22,12 @@ public class NestedClassesServlet extends HttpServlet {
         dog.setName("Fluffy");
         human.setDog(dog);
 
+        Map<String, String> map = new HashMap<>();
+        map.put("first", "First value");
+        map.put("second", "Second value");
+
         request.setAttribute("human", human);
+        request.setAttribute("map", map);
 
         RequestDispatcher requestDispatcher = request.getRequestDispatcher("NestedClassesTest.jsp");
         requestDispatcher.forward(request, response);
--- chapter 8/project/web/NestedClassesTest.jsp
+++ chapter 8/project/web/NestedClassesTest.jsp
@@ -2,8 +2,13 @@
 <body>
 <h1 align="center">Nested Classes Test</h1>
 
+<h2>Class attributes test:</h2>
 <p>Human' name: ${human.name}</p>
 <p>Dog' name: ${human.dog.name}</p>
+
+<h2>Map attributes test</h2>
+<p>First: ${map.first}</p>
+<p>Second: ${map.second}</p>
 
 </body>
 </html>

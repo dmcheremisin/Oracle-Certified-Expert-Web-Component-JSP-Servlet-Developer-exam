1d7a1797e79a57f20f3c54983ca439e6e93c9358
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2018-09-28 23:48:56 +0300

    chapter 11: war file made with maven

---
+++ chapter 11/project/src/main/java/com/example/TestServlet.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by Dmitrii on 28.09.2018.
+ */
+public class TestServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>" +
+                "<head></head>" +
+                "<body>" +
+                "<h1 align='center'> Hello from Test Servlet </h1>" +
+                "</html>");
+    }
+}
---
+++ chapter 11/project/src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>Main page</title>
+</head>
+<body>
+<h1>Welcome to our new web site</h1>
+<p>This the best site we have ever made.</p>
+</body>
+</html>
